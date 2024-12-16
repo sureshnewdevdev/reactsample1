eceaaba8864c37fb5f263b84c18628163f4f6b44
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-12-16 08:18:10 +0530

    Documents added

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return <div class="d-flex align-items-center flex-shrink-0 topbar-info">
+  <a href="https://maps.app.goo.gl/J7xb8mLhybcSLSQk6" class="me-4 text-light" target="_blank">
+    <i class="fas fa-map-marker-alt me-2 text-dark"></i>
+    India – Coimbatore, Tamil Nadu
+  </a>
+  <a href="tel:+918667311175" class="me-4 text-light" target="_blank">
+    <i class="fas fa-phone me-2 text-dark"></i>
+    +91 8667311175
+  </a>
+  <a href="mailto:contact@hapusinfotech.com" class="text-light" target="_blank">
+    <i class="fas fa-envelope me-2 text-dark"></i>
+    contact@hapusinfotech.com
+  </a>
+</div>;
 }
 
 export default Home;
